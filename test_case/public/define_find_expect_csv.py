51d4e9921c49329bfd29b37a07cfeeb44dd566c0
LSW <lsw@aituyou.com>
2017-09-20 11:22:20 +0800

    增加日志信息，结构有待优化，信息格式还待确定

--- test_case/public/define_find_expect_csv.py
+++ test_case/public/define_find_expect_csv.py
@@ -1,5 +1,6 @@
 # /usr/bin/env python
 # -*- coding:utf-8 -*-
+"""读取CSV文件，并返回CSV 行 数组 需要一个配置文件"""
 import codecs
 import csv
 
@@ -8,21 +9,31 @@ import sys
 
 from define_log import LogDefine
 
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 path=r"F:\autotest\ssssss111.csv"
-class Fwe():
-    def test_interface_by_csv(self,path=r"F:\autotest\ssssss111.csv"):
+class FindCsvFile():
+    #def __init__(self):
+        #self.path = "从配置文件获取"
+    @classmethod
+    def find_interface_info_by_csv(self, path=r"F:\autotest\ssssss111.csv"):
+    #def find_interface_info_by_csv(self):
+        #path = self.path
         LogDefine()
         rowsall =[]
-
-        with codecs.open(path,"rb") as f:
-            logging.info(u"成功打开csvw文件")
-            reader = csv.DictReader(f)
-            for i, row in enumerate(reader):
-                rowsall.append(row)
-        return rowsall
+        try:
+            logging.info(u"打开CSV文件")
+            with codecs.open(path,"rb") as f:
+                logging.info(u"成功打开csvw文件")
+                reader = csv.DictReader(f)
+                for i, row in enumerate(reader):
+                    rowsall.append(row)
+            return rowsall
+        except Exception,e:
+            logging.error("read the csv file %s wrong!!! %s ",path,e)
+            print "read the csv file %s wrong!!! %s"%(path,e)
 
 
 
