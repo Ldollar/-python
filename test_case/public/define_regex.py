25a7cdd7b3ef73ede6dc7230b2e70bac56f24252
LSW <lsw@aituyou.com>
2017-10-10 16:22:43 +0800

    字符串匹配长度，CSV文件空内容问题

--- test_case/public/define_regex.py
+++ test_case/public/define_regex.py
@@ -20,9 +20,9 @@ def find_code(text=None,rex_str=None):
 #find_code()
 @logged
 def find_message(res=None,res_str_zhongwen=None):
-    #rule = '"'+res_str_zhongwen+'":([\S\s]{0,15}),'   CSV文件是否添加双引号问题
+    #rule = '"'+res_str_zhongwen+'":([\S\s]{0,25}),'  # CSV文件是否添加双引号问题
 
-    rule = res_str_zhongwen + ':([\S\s]{0,15}),'
+    rule = res_str_zhongwen + ':([\S\s]{0,25}),'  #匹配长度需要定义，可能匹配的长度为多少，此rule匹配csv文件的assert_message 不用再添加“"”
     pattern = re.compile(rule,re.I)
     result = re.findall(pattern, res)
     if len(result)>0:
@@ -43,10 +43,11 @@ def match_string(rules=None,text=None):
         return None
 
 
-page = '{"res":[{"creator":"search","version":"1.1.1","type":2,"description":"感谢各位的收听和关注。 《微风往事》为线下多个调频广播的音乐情感伴随类节目的精编版，所有歌曲按音乐类电台的要求全部完整播放。 《微风往事》每日在地面频率播出的时间如下： 【深圳优悦FM105.7】 22:00-23:00， 【湖南优悦FM94.5】/【海口音乐FM91.6】/【 贵阳音乐FM90.9】/ 【石家庄优悦FM106.2】以上四台均为 20:00-21:00 唐山FM98.4 17：00-18：00","iconUrl":"http://fdfs.xmcdn.com/group4/M02/3C/6F/wKgDtFNHoCrAOFbbAAE2PrXoY9E149_mobile_meduim.jpg","sourceId":"249163","announcerId":"1450037"},{"creator":"search","name":"王健林 马云 周星驰 雷军 刘德华 梁凯恩 成功经验","type":2,"description":"专辑罗列 成功名人 创业 经商 销售经验 帮助同僚获得经验少走弯路","iconUrl":"http://fdfs.xmcdn.com/group19/M0A/D1/9C/wKgJJlfHvYSzvKyUAAOmsfvF7ZY514_mobile_meduim.jpg","sourceId":"5154639","announcerId":"55665669"},{"creator":"search","name":"世界童话","type":2,"description":"每天读一个","iconUrl":"","sourceId":"3244006","announcerId":"16930537"}],"message":"success","code":0}'
-page1 ='{"res": [],"message": "success","code": 0}'
-#print page1
-#z=find_message(res=page1,res_str_zhongwen="message" )
+#page = '{"res":[{"creator":"search","version":"1.1.1","type":2,"description":"感谢各位的收听和关注。 《微风往事》为线下多个调频广播的音乐情感伴随类节目的精编版，所有歌曲按音乐类电台的要求全部完整播放。 《微风往事》每日在地面频率播出的时间如下： 【深圳优悦FM105.7】 22:00-23:00， 【湖南优悦FM94.5】/【海口音乐FM91.6】/【 贵阳音乐FM90.9】/ 【石家庄优悦FM106.2】以上四台均为 20:00-21:00 唐山FM98.4 17：00-18：00","iconUrl":"http://fdfs.xmcdn.com/group4/M02/3C/6F/wKgDtFNHoCrAOFbbAAE2PrXoY9E149_mobile_meduim.jpg","sourceId":"249163","announcerId":"1450037"},{"creator":"search","name":"王健林 马云 周星驰 雷军 刘德华 梁凯恩 成功经验","type":2,"description":"专辑罗列 成功名人 创业 经商 销售经验 帮助同僚获得经验少走弯路","iconUrl":"http://fdfs.xmcdn.com/group19/M0A/D1/9C/wKgJJlfHvYSzvKyUAAOmsfvF7ZY514_mobile_meduim.jpg","sourceId":"5154639","announcerId":"55665669"},{"creator":"search","name":"世界童话","type":2,"description":"每天读一个","iconUrl":"","sourceId":"3244006","announcerId":"16930537"}],"message":"success","code":0}'
+#page1 ='{"res": [],"message": "success","code": 0}'
+#page2 ='{"code": -916,"message": "no available ota","ch": "无可用更新"}'
+#print page2
+#z=find_message(res=page2,res_str_zhongwen="message" )
 #print z
 #a=match_string(rules="random\\((\S+)\\)",text="random(1,2,3)")
 #print a
